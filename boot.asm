e3e84d473572c6dd3d3b0c636f1d10857ab37c8d
Lukas Fend <lukas.fend@outlook.com>
2025-12-10 12:09:20 +0100

    Protected Mode activated

--- boot.asm
+++ boot.asm
@@ -67,6 +67,14 @@ gdt_descriptor:
 
 [BITS 32]
 load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
 
 error_message: db 'Failed to load sector', 0
